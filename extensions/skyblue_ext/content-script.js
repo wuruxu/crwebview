c5c77519f06966328f18f7f4e93e068a5a67aa88
wuruxu <wrxzzj@gmail.com>
2022-06-10 14:01:34 +0800

    add skyblue extension

---
+++ extensions/skyblue_ext/content-script.js
@@ -0,0 +1,3 @@
+// color value : https://developer.mozilla.org/zh-CN/docs/Web/CSS/color_value
+console.log("javascript run_at document_end, set background skyblue");
+document.body.style.background = 'skyblue';
