00ac17bd233d8fb02bffece32386f1bece1adf7d
wuruxu <wrxzzj@gmail.com>
2024-12-10 23:11:15 +0800

    add WebView demo to crwebview

---
+++ crwebkitapp/java/com/github/webviewdemo/MainActivity.java
@@ -0,0 +1,32 @@
+package com.github.webviewdemo;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+//import android.webkit.WebView;
+import org.chromium.android_crwebview.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_webkit);
+
+        WebView mWebview = findViewById(R.id.webView);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //mWebview.loadUrl("https://www.kernel.org/");
+        mWebview.loadUrl("https://www.google.com.hk/");
+    }
+}
